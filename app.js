3c8e4ad18638491c80b0e9ea641542a4067cdcce
Aji Fatur <ajifatur32@gmail.com>
2022-06-03 10:38:04 +0700

    Add cors, update json

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const cors = require('cors');
 const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(cors({
+    origin: '*'
+}));
 
 app.get('/', (req, res) => {
     axios
@@ -11,12 +16,28 @@ app.get('/', (req, res) => {
         .then(response => {
             if(response.status === 200) {
                 const $ = cheerio.load(response.data);
+
+                // Get icons
                 let icons = [];
                 $('#content .list li').each(function(i, elem) {
                     icons[i] = $(elem).find('.name').text();
                 });
                 icons = icons.filter(n => n !== undefined);
-                res.json(icons);
+
+                // Get version
+                let version = $('footer.bd-footer p a:first-child').text();
+
+                // Get CDN URL
+                let url = $('#cdn').parent().find('code.language-html span.s').last().text();
+                url = url.replace('\"','');
+                url = url.replace('"','');
+
+                res.json({
+                    version: version,
+                    url: url,
+                    icons: icons,
+                    length: icons.length
+                });
             }
         })
         .catch(error => {
